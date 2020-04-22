a188c23c03823ccb2df0052510dfb021b23e08ee
elkinny <elkin.working@gmail.com>
2020-04-22 22:27:27 +0300

    add mongo

--- src/server.js
+++ src/server.js
@@ -1,41 +1,73 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import posts from './data/posts.json';
-import contacts from './data/contacts.json';
-import fiddles from './data/fiddles.json';
-import projects from './data/projects.json';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const getPostById = postId => {
-    return posts.data.find(post => post.id === postId);
+const getPostById = async (id, db) => {
+    return await db.collection('posts').findOne({id});
 }
 
-app.get('/api/posts', (req, res) => res.send(posts));
-app.post('/api/posts/:id/add-comment', (req, res) => {
-    const {text, username} = req.body;
-    const post = getPostById(req.params.id);
+const withDB = async (operations, res) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology: true});
+        const db = client.db('blog');
+    
+        await operations(db);
+    
+        client.close();
+    } catch(error) {
+        res.status(500).json({ error: ''+error })
+    }
+}
 
-    const comment = {text, username};
-
-    post.comments ? post.comments.push(comment) : post.comments = [comment];
-
-    res.status(200)
-        .send(`Comment from ${username} was added`);
+app.get('/api/posts', async (req, res) => {   
+    await withDB (async db => { 
+        const posts = await db.collection('posts').find();
+        res.status(200).json(posts);
+    }, res);
 });
-app.get('/api/posts/:id/get-comments', (req, res) => res.send(getPostById(req.params.id).comments));
-app.post('/api/posts/:id/upvote', (req, res) => {
-    const post = getPostById(req.params.id);
-
-    post.upvotes = post.upvotes ? +post.upvotes + 1 : 1;
-    res.status(200)
-        .send(`${postId} now has ${post.upvotes} upvotes`);
+app.get('/api/posts/:id', async (req, res) => {
+    await withDB (async db => { 
+        const post = await getPostById(req.params.id, db);
+        res.status(200).json(post ? post : {});
+    }, res);
 });
+app.post('/api/posts/:id/add-comment', async (req, res) => {
+    await withDB (async db => { 
+        const {text, username} = req.body;
+        const post = await getPostById(req.params.id, db);
+        const comments = post.comments ? [...post.comments, {text, username}] : [{text, username}];
+        
+        await db.collection('posts').updateOne({id: req.params.id}, {
+            '$set': {
+                comments
+            }
+        });
 
-app.get('/api/contacts', (req, res) => res.send(contacts));
-app.get('/api/fiddles', (req, res) => res.send(fiddles));
-app.get('/api/projects', (req, res) => res.send(projects));
+        const updatedPost = await getPostById(req.params.id, db);
+        res.status(200).json(updatedPost.comments);
+    }, res);
+});
+app.get('/api/posts/:id/get-comments', async (req, res) => {
+    await withDB (async db => { 
+        const post = await getPostById(req.params.id, db);
+        res.status(200).json(post.comments ? post.comments : []);
+    }, res);
+});
+app.post('/api/posts/:id/upvote', async (req, res) => {
+    await withDB (async db => { 
+        const post = await getPostById(req.params.id, db);
+        await db.collection('posts').updateOne({id: req.params.id}, {
+            '$set': {
+                upvotes: post.upvotes ? +post.upvotes + 1 : 1
+            }
+        })
+        const updatedPost = await getPostById(req.params.id, db);
+        res.status(200).json(updatedPost.upvotes);
+    }, res);
+});
 
 app.listen(8000, () => console.log('Listening on port 8000'));
