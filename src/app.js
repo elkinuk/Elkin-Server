f83b85a264cf02de98edc92f9687535750fa75d6
elkinny <elkin.working@gmail.com>
2020-04-26 17:10:43 +0300

    5543

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import mongoose from 'mongoose';
 
 import app from './config';
 
-app.listen(4000, () => {
-  console.log('...... Listening on port 4000! \n');
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`...... Listening on port ${port}! \n`);
 
   //mongoose.connect('mongodb://localhost/' + 'blog', {useUnifiedTopology: true, useNewUrlParser: true});
   mongoose.connect('mongodb://test:test1test1@ds145273.mlab.com:45273/heroku_lm857r9v',{useUnifiedTopology: true, useNewUrlParser: true});
