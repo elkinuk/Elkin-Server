90c5d133b20eefc66d1bf5ac37de5602a91a9a49
elkinny <elkin.working@gmail.com>
2020-04-28 14:06:34 +0300

    213313

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import app from './config';
 
 const port = process.env.PORT || 4000;
+const dev = process.env.NODE_ENV !== 'production'
 
 const appListen = handle => {
   
@@ -11,8 +12,9 @@ const appListen = handle => {
   app.listen(port, () => {
     console.log(`...... Listening on port ${port}! \n`);
 
-    mongoose.connect('mongodb://localhost/' + 'blog', {useUnifiedTopology: true, useNewUrlParser: true});
-    //mongoose.connect('mongodb://test:test1test1@ds145273.mlab.com:45273/heroku_lm857r9v',{useUnifiedTopology: true, useNewUrlParser: true});
+    const mongooseUrl = 'mongodb://' + (dev ? 'mongodb://localhost/blog' : 'mongodb://test:test1test1@ds145273.mlab.com:45273/heroku_lm857r9v');
+
+    mongoose.connect(mongooseUrl, {useUnifiedTopology: true, useNewUrlParser: true});
     const dbConnection = mongoose.connection;
     dbConnection.on('error', console.error.bind(console, 'connection error:'));
     dbConnection.once('open', () => {
