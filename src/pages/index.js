f449e440a3a0ae5c0fcfa77fd827a9d315561269
elkinny <elkin.working@gmail.com>
2020-04-30 14:46:17 +0300

    fix fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './style.scss';
-
 import Projects from './../components/organisms/Projects';
 import Description from './../components/organisms/Description';
 
