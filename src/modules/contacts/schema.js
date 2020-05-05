b3607b322d57709f80f7c17078b49d78048292f3
elkinny <elkin.working@gmail.com>
2020-05-05 18:30:01 +0300

    fix merge issue

--- src/modules/contacts/schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const projects = new mongoose.Schema({
-  url: String,
-  icon: String,
-  iconType: String
-});
-
-module.exports = projects;
--- src/modules/fiddles/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import req from './controlles.js';
-
-const router = express.Router();
-
-router.route('/')
-  .get((...args) => req.getAllData(...args))
-
-module.exports = router
