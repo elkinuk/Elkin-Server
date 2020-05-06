582f85661d7b7261770a71ab2879a5fc8a8d0eb7
elkinny <elkin.working@gmail.com>
2020-05-07 00:55:54 +0300

    Add fiddles

---
+++ src/components/molecules/Fiddle/Fiddle.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+import { Article, LinkSimple } from './../../atoms/shared';
+
+import style from './style.module.scss'
+
+const Fiddle = ({keyHash, config, text, title}) => {
+  const getUrl = () => `//jsfiddle.net/elkin_uk/${keyHash}/embedded/${config}/dark/`;
+
+  return ( 
+    <div className={ style.fiddle }>
+      <Article text={ text } title={ title } className={ style["fiddle__article"] } />
+      <LinkSimple href={ getUrl() }>
+        Fullscreen fiddle
+      </LinkSimple>
+      <div className={ style["fiddle__item"] }>
+        <iframe
+          className={ style["fiddle__iframe"] }
+          width="100%"
+          height="300"
+          src={ getUrl() }
+          allowfullscreen="allowfullscreen"
+          allowpaymentrequest
+          frameborder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+ 
+export default Fiddle;
