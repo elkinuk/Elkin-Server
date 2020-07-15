5e34bb31e666c71e0a170d654532f825bfee23f9
elkinny <elkin.working@gmail.com>
2020-07-16 00:17:11 +0300

    refactoring

--- src/components/atoms/About/About.js
+++ src/components/atoms/About/About.js
@@ -6,13 +6,10 @@ import style from './style.module.scss';
 
 const About = () => {
   const text = [
-      `Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro vitae
-    accusamus, dignissimos possimus commodi quas, repudiandae officia dolorum
-    quisquam nisi libero culpa doloribus sed. Voluptas dignissimos rerum
-    corrupti velit quod. Lorem ipsum dolor sit amet consectetur adipisicing
-    elit. Porro vitae accusamus, dignissimos possimus commodi quas,
-    repudiandae officia dolorum quisquam nisi libero culpa doloribus sed.
-    Voluptas dignissimos rerum corrupti velit quod.`,
+      `Hello, there! My name is Victoria and I'm a front-end developer. This page is 
+      my CV, my bio, my everything, lets say. And as you already propably noticed, it is not
+      so much stuff here going on. It is because this website is still WIP, but anyway, I'm
+      really thankfull to see you here!`,
       `Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro vitae
     accusamus, dignissimos possimus commodi quas, repudiandae officia dolorum
     quisquam nisi libero culpa doloribus sed. Voluptas dignissimos rerum
--- src/components/base/Header/Header.js
+++ src/components/base/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   return ( 
     <header className={ style.header }>
       <Logo />
-      <Navigation />
+      {/* <Navigation /> */}
     </header>
   );
 }
