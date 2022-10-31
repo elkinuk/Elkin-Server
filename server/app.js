f25ce71b03f797115191efbcde9cff987ee34fde
elkinny <elkin.life@gmail.com>
2022-10-31 21:36:50 +0000

    update backend

--- server/app.js
+++ server/app.js
@@ -1,5 +1,3 @@
-import mongoose from 'mongoose';
-
 import app from './config';
 
 const dev = process.env.NODE_ENV !== 'production'
--- server/modules/contacts/routes.js
+++ server/modules/contacts/routes.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import req from './controlles.js';
+import data from './../../db/contacts';
 
 const router = express.Router();
 
 router.route('/')
-  .get((...args) => req.getAllData(...args))
+  .get((req, res, next) => {
+    try{
+      res.send(data.data)
+    } catch(error) {
+      next(error);
+    }
+  })
 
 module.exports = router
